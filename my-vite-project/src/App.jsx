fd22b2b78a6cf6656c5da4cc1bd6355014711cf4
Kengan <jaimeperaza2001@gmail.com>
2024-05-25 16:29:00 -0500

    fix: Se corrige funcionamiento de react-flow

--- my-vite-project/src/App.jsx
+++ my-vite-project/src/App.jsx
@@ -1,4 +1,7 @@
-import ReactFlow, { MiniMap, Controls, Background } from "react-flow-renderer";
+import  { useEffect, useState } from 'react';
+import ReactFlow, { useNodesState, useEdgesState } from 'reactflow';
+import 'reactflow/dist/style.css';
+import './updatenode.css';
 
 const data = {
   Organizacion: "ADL",
@@ -99,21 +102,95 @@ const generateNodesAndEdges = (data) => {
 
 const { nodes, edges } = generateNodesAndEdges(data);
 
-const App = () => (
-  <div style={{ height: "100vh" }}>
-    <ReactFlow
-      elements={[...nodes, ...edges]}
-      nodesDraggable={false}
-      nodesConnectable={false}
-      elementsSelectable={false}
-      snapToGrid={true}
-      snapGrid={[15, 15]}
-    >
-      <MiniMap />
-      <Controls />
-      <Background />
-    </ReactFlow>
-  </div>
-);
+const UpdateNode = () => {
+  const [nods, setNodes, onNodesChange] = useNodesState(nodes);
+  const [edgs, setEdges, onEdgesChange] = useEdgesState(edges);
 
-export default App;
+  const [nodeName, setNodeName] = useState('Node 1');
+  const [nodeBg, setNodeBg] = useState('#eee');
+  const [nodeHidden, setNodeHidden] = useState(false);
+
+  useEffect(() => {
+    setNodes((nds) =>
+      nds.map((node) => {
+        if (node.id === '1') {
+          // it's important that you create a new object here
+          // in order to notify react flow about the change
+          node.data = {
+            ...node.data,
+            label: nodeName,
+          };
+        }
+
+        return node;
+      })
+    );
+  }, [nodeName, setNodes]);
+
+  useEffect(() => {
+    setNodes((nds) =>
+      nds.map((node) => {
+        if (node.id === '1') {
+          // it's important that you create a new object here
+          // in order to notify react flow about the change
+          node.style = { ...node.style, backgroundColor: nodeBg };
+        }
+
+        return node;
+      })
+    );
+  }, [nodeBg, setNodes]);
+
+  useEffect(() => {
+    setNodes((nds) =>
+      nds.map((node) => {
+        if (node.id === '1') {
+          // when you update a simple type you can just update the value
+          node.hidden = nodeHidden;
+        }
+
+        return node;
+      })
+    );
+    setEdges((eds) =>
+      eds.map((edge) => {
+        if (edge.id === 'e1-2') {
+          edge.hidden = nodeHidden;
+        }
+
+        return edge;
+      })
+    );
+  }, [nodeHidden, setNodes, setEdges]);
+
+  return (
+    <ReactFlow
+      nodes={nods}
+      edges={edgs}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      minZoom={0.2}
+      maxZoom={4}
+      attributionPosition="bottom-left"
+    >
+      <div className="updatenode__controls">
+        <label>label:</label>
+        <input value={nodeName} onChange={(evt) => setNodeName(evt.target.value)} />
+
+        <label className="updatenode__bglabel">background:</label>
+        <input value={nodeBg} onChange={(evt) => setNodeBg(evt.target.value)} />
+
+        <div className="updatenode__checkboxwrapper">
+          <label>hidden:</label>
+          <input
+            type="checkbox"
+            checked={nodeHidden}
+            onChange={(evt) => setNodeHidden(evt.target.checked)}
+          />
+        </div>
+      </div>
+    </ReactFlow>
+  );
+};
+
+export default UpdateNode;
